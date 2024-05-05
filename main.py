44cf05e54e113000c4e0fd092c763d32b2772365
Amin Fazlali <amin@fazlali.net>
2024-05-05 15:48:17 +0330

    bug fix

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+
 from fastapi import FastAPI, Request, Depends, HTTPException
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from starlette.staticfiles import StaticFiles
@@ -5,18 +7,15 @@ from starlette.status import HTTP_401_UNAUTHORIZED
 
 from core import scraper, extractor
 
-users_db = {
-    "admin": "EoIi9Kw57L5MESCjGcRbIB5RPabjlgSg",
-    "a.rafiee": "4uQs9fq4OF7kGTH3wNrNtIw3lYJgITO9"
-}
-
 security = HTTPBasic()
 
 
 def authenticate_user(credentials: HTTPBasicCredentials = Depends(security)):
-    if credentials.username in users_db:
-        correct_password = users_db[credentials.username]
-        if credentials.password == correct_password:
+    with open('/users.json', 'r') as f:
+        users = json.load(f)
+    if credentials.username in users:
+        password = users[credentials.username]
+        if credentials.password == password:
             return credentials.username
 
     raise HTTPException(
