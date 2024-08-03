8fdd50e6c33fdd7f52e08abc00b82bdae772f61c
Amin Fazlali <amin@fazlali.net>
2024-08-03 15:04:00 +0330

    bug fix

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import json
+import os.path
 
 from fastapi import FastAPI, Request, Depends, HTTPException
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from fastapi.responses import FileResponse
+from starlette.responses import JSONResponse
 from starlette.status import HTTP_401_UNAUTHORIZED
 
 from core import scraper, extractor
@@ -31,13 +33,37 @@ app = FastAPI(dependencies=[Depends(authenticate_user)])
 @app.post("/scrape")
 async def scrape(request: Request):
     body = await request.json()
-    return scraper.scrape([body['url']], body['selectors'])[0]
+    products = scraper.scrape(body['urls'], body['selectors'])
+    return {'products': products}
 
 
 @app.post("/extract")
 async def extract(request: Request):
     body = await request.json()
-    return extractor.extract(body['start_urls'], body['config'])
+    product_urls = extractor.extract(body['start_urls'], body['config'])
+    return {'product_urls': product_urls}
+
+
+@app.post("/websites/{domain}")
+async def extract(request: Request, domain, username=Depends(authenticate_user)):
+    body = await request.json()
+    body['domain'] = domain
+    if not os.path.exists(f'./db/{username}'):
+        os.makedirs(f'./db/{username}')
+        with open(f'/db/{username}/{domain}.json', 'w') as f:
+            json.dump(body, f)
+
+    return {'website': body}
+
+
+@app.get("/websites/{domain}")
+async def extract(request: Request, domain: str, username=Depends(authenticate_user)):
+    if os.path.isfile(f'./db/{username}/{domain}.json'):
+        with open(f'./db/{username}/{domain}.json', 'r') as f:
+            return {
+                'website': json.load(f)
+            }
+    return JSONResponse({'website': None}, status_code=404)
 
 
 @app.get('/')
