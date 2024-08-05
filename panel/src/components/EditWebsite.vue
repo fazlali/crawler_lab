2a49a3c34c48ddb572a4cfd15d4ecaa93409b059
Amin Fazlali <amin@fazlali.net>
2024-08-05 16:19:15 +0330

    bug fix

--- panel/src/components/EditWebsite.vue
+++ panel/src/components/EditWebsite.vue
@@ -166,7 +166,7 @@ async function scrapeTest(url) {
               <div class="grid grid-cols-[1fr_auto] gap-2 mb-3">
                 <div>
                   <input
-                      class="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300"
+                      class="block w-full rounded-md border-0 py-1.5 px-7 text-gray-900 ring-1 ring-inset ring-gray-300"
                       type="text"
                       v-model="scrapeTestUrl"
                   >
