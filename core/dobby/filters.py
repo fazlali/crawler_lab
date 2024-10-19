b0dc462894fbec9b6214f861b2f4f9071339f69d
Amin Fazlali <amin@fazlali.net>
2024-10-19 17:27:16 +0330

    bug fix

--- core/dobby/filters.py
+++ core/dobby/filters.py
@@ -42,69 +42,10 @@ def price_filter(price: str):
     persian_digits = '۰۱۲۳۴۵۶۷۸۹'
     english_digits = '0123456789'
     # comma, period, thousands separator, decimal separator, space
-    unwanted_characters = ',.٬٫ '
+    unwanted_characters = ',٬٫ /'
 
     table = str.maketrans(persian_digits, english_digits, unwanted_characters)
     price = price.translate(table)
     digits = re.search(r'\d+', price)
     if digits:
         return int(digits.group(0))
-
-def currency_filter(currency: str):
-    """
-    Clean up a currency string by removing any non-word characters and keeping the first word.
-
-    Parameters:
-    - currency (str): The currency string to be cleaned up.
-
-    Returns:
-    - str: The cleaned up currency string.
-    """
-    if not currency:
-        return ''
-
-    list_of_known_currencies = [
-        '﷼',
-        'میلیون ریال',
-        'ملیون ریال',
-        'میلیون‌ریال',
-        'ملیون‌ریال',
-        'ریال',
-        'هزارتومان',
-        'هزار تومان',
-        'تومان',
-        'rial',
-        'toman',
-        'IRT',
-        'IRR',
-    ]
-
-    for c in list_of_known_currencies:
-        if c.lower() in currency.lower():
-            return c
-
-    # remove all non-word characters
-    return re.sub(r'[\W\d]+', '', currency).strip()
-
-def currency_translate(currency: str):
-    """
-    Translate the given currency to its equivalent in Persian currency. 
-
-    Args:
-        currency (str): The currency to be translated.
-
-    Returns:
-        str: The translated currency, or the original currency if no translation is found.
-    """
-    translate_table = {
-        '﷼': 'ریال',
-        'هزار تومان': 'هزارتومان',
-        'میلیون ریال': 'میلیون‌ریال',
-        'ملیون ریال': 'میلیون‌ریال',
-        'rial': 'ریال',
-        'toman': 'تومان',
-        'irt': 'تومان',
-        'irr': 'ریال',
-    }
-
-    return translate_table.get(currency.lower(), currency)
